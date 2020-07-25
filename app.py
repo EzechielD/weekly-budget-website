19c4574dad501dab17de5652250df9fa737dd33e
EzechielD <ezechield07@gmail.com>
2020-07-25 18:21:17 -0400

    placeholder text on index; BootstrapCDN added; base template created

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+#Home page
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
