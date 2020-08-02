16b6b28af06f133d33ffffa927081540271a4509
EzechielD <ezechield07@gmail.com>
2020-08-02 18:20:53 -0400

    added Handsontable; routing from index to accounts

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
 #Home page
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+#Accounts Page
+@app.route("/accounts", methods=["POST", "GET"])
+def accounts():
+    return render_template("accounts.html")
 
 if __name__ == "__main__":
     app.run()
